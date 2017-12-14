76179100503d82e14fe43072b518130fc4e4df14
huangguangda <2397923107@qq.com>
2017-12-14 19:28:57 +0800

    194

--- app/src/main/java/cn/edu/gdmec/android/broadcasttest/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/broadcasttest/MainActivity.java
@@ -8,6 +8,8 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
+        Button button = ( Button ) findViewById ( R.id.button );
+        button.setOnClickListener ( new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent ( "cn.edu.gdmec.android.broadcasttest.MY_BROADCAST" );
+                sendBroadcast ( intent );
+            }
+        } );
+
         intentFilter = new IntentFilter (  );
         intentFilter.addAction ( "android.net.conn.CONNECTIVITY_CHANGE" );
         networkChangeReceiver = new NetworkChangeReceiver();
