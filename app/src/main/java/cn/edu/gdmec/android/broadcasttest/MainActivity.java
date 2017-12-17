d6c436474c7acf8e8849db5d22dc18a806324994
huangguangda <2397923107@qq.com>
2017-12-17 17:42:07 +0800

    完成

--- app/src/main/java/cn/edu/gdmec/android/broadcasttest/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/broadcasttest/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,31 +15,50 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private IntentFilter intentFilter;
+    private LocalReceiver localReceiver;
+    private LocalBroadcastManager localBroadcastManageer;
+
     private NetworkChangeReceiver networkChangeReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
+        localBroadcastManageer = LocalBroadcastManager.getInstance ( this );
+
         Button button = ( Button ) findViewById ( R.id.button );
         button.setOnClickListener ( new View.OnClickListener () {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent ( "cn.edu.gdmec.android.broadcasttest.MY_BROADCAST" );
+                Intent intent = new Intent ( "cn.edu.gdmec.android.broadcasttest.LOCAL_BROADCAST" );
+                localBroadcastManageer.sendBroadcast ( intent );
+                /*  Intent intent = new Intent ( "cn.edu.gdmec.android.broadcasttest.MY_BROADCAST" );
                 sendOrderedBroadcast ( intent, null );
-                sendBroadcast ( intent );
+                sendBroadcast ( intent );*/
             }
         } );
 
         intentFilter = new IntentFilter (  );
-        intentFilter.addAction ( "android.net.conn.CONNECTIVITY_CHANGE" );
+        intentFilter.addAction ( "cn.edu.gdmec.android.broadcasttest.LOCAL_BROADCAST" );
+        localReceiver = new LocalReceiver();
+        localBroadcastManageer.registerReceiver ( localReceiver, intentFilter );
+    /*    intentFilter.addAction ( "android.net.conn.CONNECTIVITY_CHANGE" );
         networkChangeReceiver = new NetworkChangeReceiver();
-        registerReceiver ( networkChangeReceiver, intentFilter );
+        registerReceiver ( networkChangeReceiver, intentFilter );*/
     }
     @Override
     protected void onDestroy(){
         super.onDestroy ();
-        unregisterReceiver ( networkChangeReceiver );
+        //unregisterReceiver ( networkChangeReceiver );
+        localBroadcastManageer.unregisterReceiver ( localReceiver );
+    }
+
+    class LocalReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText ( context, "received local broadcast", Toast.LENGTH_SHORT ).show ();
+        }
     }
     class NetworkChangeReceiver extends BroadcastReceiver{
         @Override
